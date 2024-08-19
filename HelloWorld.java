26f10174786ea0b442d947422198465a4274de1e
rohineerakte <rohinee.rakte@gmail.com>
2024-08-19 16:17:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(".......Welcome to jenkins training  test...");
+        System.out.println(".......Trying webhooks...");
     }
 }
